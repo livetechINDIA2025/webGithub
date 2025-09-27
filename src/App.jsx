5a955866fd91239f106817b290221126c433e44e
LivetechInida <harshadmehta4101990@gmail.com>
2025-09-27 12:10:23 +0530

    added person details

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 export default function App() {
+  const [person, setPerson] = useState({
+    username: 'john',
+    email: "john@gmail.com"
+  })
   return (
     <>
       <h1>App Component</h1>
       <h2>New Line have been made using github</h2>
       <p>This is a Sample Sentence</p>
+      <h2>Person Name: {person?.username}</h2>
+      <h2>Person Email: {person?.email}</h2>
     </>
   )
 }
